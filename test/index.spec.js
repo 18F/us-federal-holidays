d5732cabad0d1f462ba868dc84cb5174910ecb5d
Jesse CreateThis <jesse@createthis.com>
2018-08-30 10:27:50 -0400

    Keep it DRY

--- test/index.spec.js
+++ test/index.spec.js
@@ -26,42 +26,38 @@ describe('fedHolidays', () => {
       });
     });
     describe('federal bank holidays', () => {
+      let assertTrueWithShiftedSaturdays = (date_s) => {
+        let date = new Date(`${date_s} 00:00:00`);
+        let dateUTC = new Date(`${date_s} 00:00:00 UTC`);
+        let result = fedHolidays.isAHoliday(date);
+        result.should.be.true();
+        let resultUTC = fedHolidays.isAHolidayUTC(dateUTC);
+        resultUTC.should.be.true();
+      }
+      let assertFalseWithShiftedSaturdays = (date_s) => {
+        let date = new Date(`${date_s} 00:00:00`);
+        let dateUTC = new Date(`${date_s} 00:00:00 UTC`);
+        let result = fedHolidays.isAHoliday(date);
+        result.should.be.false();
+        let resultUTC = fedHolidays.isAHolidayUTC(dateUTC);
+        resultUTC.should.be.false();
+      }
       describe('observing Saturday on previous Friday', () => {
         it('should return true for the day before Independence Day, Friday 2020-07-03', async () => {
           let date_s = '2020-07-03';
-          let date = new Date(`${date_s} 00:00:00`);
-          let dateUTC = new Date(`${date_s} 00:00:00 UTC`);
-          let result = fedHolidays.isAHoliday(date);
-          result.should.be.true();
-          let resultUTC = fedHolidays.isAHolidayUTC(dateUTC);
-          resultUTC.should.be.true();
+          assertTrueWithShiftedSaturdays(date_s);
         });
         it('should return false for Independence Day, Saturday 2020-07-04', async () => {
           let date_s = '2020-07-04';
-          let date = new Date(`${date_s} 00:00:00`);
-          let dateUTC = new Date(`${date_s} 00:00:00 UTC`);
-          let result = fedHolidays.isAHoliday(date);
-          result.should.be.false();
-          let resultUTC = fedHolidays.isAHolidayUTC(dateUTC);
-          resultUTC.should.be.false();
+          assertFalseWithShiftedSaturdays(date_s);
         });
         it('should return true for the day before New Years Day, Friday 2021-12-31', async () => {
           let date_s = '2021-12-31';
-          let date = new Date(`${date_s} 00:00:00`);
-          let dateUTC = new Date(`${date_s} 00:00:00 UTC`);
-          let result = fedHolidays.isAHoliday(date);
-          result.should.be.true();
-          let resultUTC = fedHolidays.isAHolidayUTC(dateUTC);
-          resultUTC.should.be.true();
+          assertTrueWithShiftedSaturdays(date_s);
         });
         it('should return false for New Years Day, Saturday 2022-01-01', async () => {
           let date_s = '2022-01-01';
-          let date = new Date(`${date_s} 00:00:00`);
-          let dateUTC = new Date(`${date_s} 00:00:00 UTC`);
-          let result = fedHolidays.isAHoliday(date);
-          result.should.be.false();
-          let resultUTC = fedHolidays.isAHolidayUTC(dateUTC);
-          resultUTC.should.be.false();
+          assertFalseWithShiftedSaturdays(date_s);
         });
       });
     });
